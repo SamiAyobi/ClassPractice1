c7334358e377d69b6d04a9e3657af00eb81fe2be
unknown <sami.prt@gmail.com>
2022-05-10 16:51:06 -0700

    CHanges to file

--- src/NewClass.java
+++ src/NewClass.java
@@ -2,8 +2,14 @@
 public class NewClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	int a=20;
+	int b=10;
+	int c= a+b;
+	System.out.println(c);
 
-	}
+		
+		
+		
+}
 
 }
